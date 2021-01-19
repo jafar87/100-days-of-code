6e7a2affdc77907f7e30050c2b4ff0914591f089
jafar87 <jafarcodoid@gmail.com>
2021-01-19 11:50:54 +0530

    first commit

---
+++ untitled/src/Abstraction.java
@@ -0,0 +1,33 @@
+
+
+//    *Abstraction can be achieved with either abstract classes or interfaces
+//    *The abstract keyword is a non-access modifier, used for classes and methods
+
+//    *Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
+
+//    *Abstract method: can only be used in an abstract class, and it does not have a body. The body is provided by the subclass (inherited from).
+//   *It can have constructors and static methods also.
+
+
+    // Abstract class
+abstract class Shape{
+    abstract void draw();
+}
+
+//In real scenario, implementation is provided by others i.e. unknown by end user
+class Rectangle extends Shape {
+    void draw(){System.out.println("drawing rectangle");}
+}
+
+class Circle1 extends Shape {
+    void draw(){System.out.println("drawing circle");}
+}
+
+//In real scenario, method is called by programmer or user
+class TestAbstraction1{
+    public static void main(String args[]){
+        Shape s=new Circle1();//In a real scenario, object is provided through method, e.g., getShape() method
+        s.draw();
+    }
+}
+
